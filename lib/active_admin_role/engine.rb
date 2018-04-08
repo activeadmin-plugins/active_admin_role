ff720628fe2fd8b6a8e5bfdeee61015c46731651
Yoshiyuki Hirano <yhirano@me.com>
2018-04-08 11:44:57 +0900

    Refactor ActiveAdminRole::Engine

--- lib/active_admin_role/engine.rb
+++ lib/active_admin_role/engine.rb
@@ -4,11 +4,15 @@ module ActiveAdminRole
   class Engine < ::Rails::Engine
     initializer "active_admin_role" do
       ActiveSupport.on_load :active_record do
-        ActiveRecord::Base.send :extend, ::ActiveAdminRole::Model
+        require "active_admin_role/monkey/active_record_dsl"
+        extend ActiveAdminRole::ActiveRecordDsl
       end
 
       ActiveSupport.on_load :after_initialize do
-        ::ActiveAdmin::ResourceController.send :include, ::ActiveAdminRole::ActiveAdmin::ResourceController
+        require "active_admin_role/monkey/active_admin_dsl"
+        require "active_admin_role/monkey/active_admin_controller"
+        ActiveAdmin::DSL.send :include, ActiveAdminRole::ActiveAdminDsl
+        ActiveAdmin::ResourceController.send :include, ActiveAdminRole::ActiveAdminController
       end
     end
   end
