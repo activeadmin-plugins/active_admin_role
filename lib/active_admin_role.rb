3798897ca9b26970788ad71a1eb4ab80dd180b28
Yoshiyuki Hirano <yhirano@me.com>
2018-04-08 11:42:44 +0900

    Refactor ActiveAdminRole::Engine

--- lib/active_admin_role.rb
+++ lib/active_admin_role.rb
@@ -1,12 +1,4 @@
 require "active_admin"
-require "active_admin_role/active_admin/dsl"
-require "active_admin_role/active_admin/resource_controller"
-require "active_admin_role/can_can/ability"
-require "active_admin_role/config"
-require "active_admin_role/engine"
-require "active_admin_role/manageable_resource"
-require "active_admin_role/model"
-require "active_admin_role/role_based_authorizable"
 
 module ActiveAdminRole
   def self.configure
@@ -14,8 +6,9 @@ module ActiveAdminRole
   end
 
   def self.config
-    @_config ||= Config.new
+    @config ||= Config.new
   end
 end
 
-::ActiveAdmin::DSL.send :include, ActiveAdminRole::ActiveAdmin::DSL
+require "active_admin_role/config"
+require "active_admin_role/engine" if defined?(Rails)
